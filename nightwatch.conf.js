96710d7de7dd646babbbb3b25d773593a0ff81a1
Krzysztof <krzysztof.kolodziejczyk21cn@gmail.com>
2022-04-11 12:40:10 +0200

    Update to version 2*

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -2,7 +2,7 @@ const packageJson = require('./package.json');
 const screenshotPath = './screenshots/' + packageJson.version + '/';
 
 const config = {
-	src_folders: ['tests/multiformTests.js'], // 'tests'
+	src_folders: ['tests'], // 'tests'
 	output_folder: './reports/',
 	custom_commands_path: './commands/',
 	custom_assertions_path: './assertions/',
--- tests/demoTests.js
+++ tests/demoTests.js
@@ -1,5 +1,3 @@
-const loginPage = require('../pages/loginPage/loginPage.js').LoginPage;
-
 module.exports = {
 	afterEach: function (browser, done) {
 		browser.end(function () {
@@ -7,50 +5,11 @@ module.exports = {
 		});
 	},
 
-	'Demo multiform test': (browser) => {
-		browser
-			.url(
-				'https://www.way2automation.com/angularjs-protractor/multiform/#/form/profile'
-			)
-		
+	'Demo test': (browser) => {
+		browser.url(
+			'http://www.way2automation.com/angularjs-protractor/registeration/#/login'
+		);
 
-
-			
-		browser.assert.title('Protractor practice website - Multiform');
-		
+		browser.assert.titleEquals('Protractor practice website - Registration');
 	},
-
-	// 'Demo login test': (browser) => {
-	// 	browser.url(
-	// 		'http://www.way2automation.com/angularjs-protractor/registeration/#/login'
-	// 	);
-	// 	new loginPage().login();
-
-	// 	browser.assert.title('Protractor practice website - Registration');
-	// },
-
-	// 'Demo logout test': (browser) => {
-	// 	browser
-	// 		.url(
-	// 			'http://www.way2automation.com/angularjs-protractor/registeration/#/login'
-	// 		)
-
-	// 		.clearValue('#username')
-	// 		.setValue('#username', 'angular')
-	// 		.clearValue('#password')
-	// 		.setValue('#password', 'password')
-	// 		.clearValue('#formly_1_input_username_0')
-	// 		.setValue('#formly_1_input_username_0', 'Przykładowy użytkownik')
-
-	// 		.click('.btn.btn-danger');
-
-	// 	if (browser.assert.containsText('.ng-scope', 'Home')) {
-	// 		browser
-	// 			.click('p.ng-scope:nth-child(3) > a:nth-child(1)')
-	// 			// .pause(5000)
-	// 			.assert.urlContains(
-	// 				'www.way2automation.com/angularjs-protractor/registeration/#/login'
-	// 			);
-	// 	}
-	// },
 };
